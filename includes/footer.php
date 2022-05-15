5c041ca001f4f78b7a5f91e011bdab6ce0289041
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-15 20:42:20 +0930

    setup frontend panel and login signup form

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,8 @@
-</main>
-<footer class="py-4 bg-light mt-auto">
-    <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2022</div>
-            <div>
-                <a href="#">Privacy Policy</a>
-                &middot;
-                <a href="#">Terms &amp; Conditions</a>
-            </div>
-        </div>
-    </div>
-</footer>
-</div>
-</div>
+
+<script scr="assets/js/jquery.min.js"></script>
+<script scr="assets/js/bootstrap5.bundle.min.js"></script>
+<script scr="assets/js/scripts.js"></script>
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+include('includes/header.php');
+?>
+
+<?php
+include('includes/navbar.php');
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Login</h4>
+                    </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                        <label for="">Email</label>
+                        <input type="email" placeholder="Enter Email Address" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Password</label>
+                        <input type="password" placeholder="Enter Password" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <button type="submit" class="btn btn-primary">Login Now</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php
+include('includes/footer.php');
+?>
