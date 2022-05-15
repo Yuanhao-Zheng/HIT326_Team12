9682a273f233445a8a59091fc0e31c6a264078a1
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-15 17:50:37 +0930

    using start bootstrap template
    https://startbootstrap.com/template/sb-admin

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard - SB Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+</head>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+<body class="sb-nav-fixed">
+
+    <?php
+    include "includes/navbar-top.php";
+    ?>
+
+
+    <div id="layoutSidenav">
+
+        <?php
+        include "includes/sidebar.php";
+        ?>
+
+        <div id="layoutSidenav_content">
+            <main>
