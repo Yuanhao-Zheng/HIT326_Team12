2c0abc395f18bcd486ecf3a55cb40ffd3b1f7d76
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-16 22:58:09 +0930

    edit updated registered user data

---
+++ admin/code.php
@@ -0,0 +1,40 @@
+<?php
+include "authentication.php";
+?>
+
+<?php
+
+if(isset($_POST['update_user']))
+{
+    $user_id = $_POST['user_id'];
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    $user_role = $_POST['user_role'];
+    $status = $_POST['status'] == true ? '1':'0';
+
+    $query = "UPDATE users SET user_firstname='{$user_firstname}', user_lastname='{$user_lastname}', user_email='{$user_email}', user_password='{$user_password}', user_role='{$user_role}', status='{$status}' WHERE user_id='$user_id' ";
+
+    $query_run = mysqli_query($connection, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Updated Successfully";
+        header('Location: view-register.php');
+        exit(0);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
--- admin/view-register.php
+++ admin/view-register.php
@@ -17,67 +17,68 @@ include "includes/header.php";
     <div class="row">
 
         <div class="col-md-12">
+            <?php include('message.php'); ?>
             <div class="card">
                 <div class="card-header">
                     <h4>Registered User</h4>
                 </div>
-                <div class="card-body"></div>
+                <div class="card-body">
 
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Roles</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $query = "SELECT * FROM users";
-                        $query_run = mysqli_query($connection, $query);
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Roles</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $query = "SELECT * FROM users";
+                            $query_run = mysqli_query($connection, $query);
 
 
-                        if (mysqli_num_rows($query_run) > 0) {
-                            foreach ($query_run as $row) {
-                        ?>
+                            if (mysqli_num_rows($query_run) > 0) {
+                                foreach ($query_run as $row) {
+                            ?>
+                                    <tr>
+                                        <td><?= $row['user_id']; ?></td>
+                                        <td><?= $row['user_firstname']; ?></td>
+                                        <td><?= $row['user_lastname']; ?></td>
+                                        <td><?= $row['user_email']; ?></td>
+                                        <td>
+                                            <?php
+                                            if ($row['user_role'] == '1') {
+                                                echo 'Admin';
+                                            } elseif ($row['user_role'] == '0') {
+                                                echo 'User';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td><a href="register-edit.php?user_id=<?= $row['user_id']; ?>" class="btn btn-secondary">Edit</td>
+                                        <td><button type="button" class="btn btn-danger">Delete</button></td>
+                                    </tr>
+                                <?php
+                                }
+                            } else {
+                                ?>
                                 <tr>
-                                    <td><?= $row['user_id']; ?></td>
-                                    <td><?= $row['user_firstname']; ?></td>
-                                    <td><?= $row['user_lastname']; ?></td>
-                                    <td><?= $row['user_email']; ?></td>
-                                    <td>
-                                        <?php 
-                                        if($row['user_role'] == '1'){
-                                            echo 'Admin';
-                                        }elseif($row['user_role'] == '0'){
-                                            echo 'User';
-                                        }
-                                        ?>
-                                </td>
-                                    <td><a href="edit-register.php" class="btn btn-secondary">Edit</td>
-                                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                                    <td colspan="6">No Record Found</td>
                                 </tr>
+
                             <?php
                             }
-                        } else {
+
                             ?>
-                            <tr>
-                                <td colspan="6">No Record Found</td>
-                            </tr>
 
-                        <?php
-                        }
+                        </tbody>
+                    </table>
 
-                        ?>
-
-                    </tbody>
-                </table>
-
-
+                </div>
 
             </div>
         </div>
@@ -86,14 +87,17 @@ include "includes/header.php";
 
     </div>
 
-    <!-- footer -->
+</div>
 
-    <?php
-    include "includes/footer.php";
-    ?>
 
-    <!-- scripts -->
+<!-- footer -->
 
-    <?php
-    include "includes/scripts.php";
-    ?>
+<?php
+include "includes/footer.php";
+?>
+
+<!-- scripts -->
+
+<?php
+include "includes/scripts.php";
+?>
