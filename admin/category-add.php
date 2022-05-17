9a2508247d34066df8cab1ad5d6d0e4e21b56399
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-17 22:09:44 +0930

    unit add - create table and insert data in db

---
+++ admin/category-add.php
@@ -0,0 +1,87 @@
+<?php
+include "authentication.php";
+?>
+
+<!-- header -->
+<?php
+include "includes/header.php";
+?>
+
+<div class="container-fluid px-4">
+    <h4 class="mt-4">Units</h4>
+
+    <div class="row mt-4">
+
+        <div class="col-md-12">
+            <?php include('message.php'); ?>
+            <div class="card">
+                <div class="card-header">
+                    <h4>Add Unit
+                        <a href="unit-view.php" class="btn btn-danger float-end">Back</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <form action="code.php" method="POST">
+                        <div class="row">
+                            <div class="col-md-12 mb-3">
+                                <label for="">Unit Code</label>
+                                <input type="text" name="unit_code" required class="form-control">
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <label for="">Unit Name</label>
+                                <input type="text" name="unit_name" required class="form-control">
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <label for="">Unit Year</label>
+                                <input type="text" name="unit_year" required class="form-control">
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <label for="">Unit Semester</label>
+                                <select name="unit_semester" required class="form-control">
+                                    <option value="">--Select Role--</option>
+                                    <option value="1">Semester 1</option>
+                                    <option value="0">Semester 2</option>
+                                </select>
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <label for="">Navbar Status</label>
+                                <input type="checkbox" name="navbar_status" width="70px" height="70px">
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <label for="">Status</label>
+                                <input type="checkbox" name="status" width="70px" height="70px">
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <button type="submit" name="unit_add" class="btn btn-primary">Save Unit</button>
+                            </div>
+                        </div>
+                    </form>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- footer -->
+
+<?php
+include "includes/footer.php";
+?>
+
+<!-- scripts -->
+
+<?php
+include "includes/scripts.php";
+?>
--- admin/code.php
+++ admin/code.php
@@ -4,6 +4,36 @@ include "authentication.php";
 
 <?php
 
+if(isset($_POST['unit_add']))
+{
+    $unit_code = $_POST['unit_code'];
+    $unit_name = $_POST['unit_name'];
+    $unit_year = $_POST['unit_year'];
+    $unit_semester = $_POST['unit_semester'] == true ? '1':'0'; //can add semester 1,2 and summer
+    $navbar_status = $_POST['navbar_status'] == true ? '1':'0';
+    $status = $_POST['status'] == true ? '1':'0';
+
+    $query = "INSERT INTO units (unit_code,unit_name,unit_year,unit_semester,navbar_status,status) VALUES 
+    ('$unit_code','$unit_name','$unit_year','$unit_semester','$navbar_status','$status')";
+
+    $query_run = mysqli_query($connection, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Unit Added Successfully";
+        header('Location: register-add.php');
+        exit(0);
+    }else
+    {
+        $_SESSION['message'] = "Something went wrong";
+        header('Location: register-add.php');
+        exit(0);
+    }
+
+}
+
+
+
 if(isset($_POST['user_delete']))
 {
     $user_id = $_POST['user_delete'];
