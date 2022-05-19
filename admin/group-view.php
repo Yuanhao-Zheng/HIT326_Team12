a1f0887bb10e05894d928bc60e7cfb7c089d038b
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-19 22:17:37 +0930

    fetch group data from db

---
+++ admin/group-view.php
@@ -0,0 +1,100 @@
+<?php
+include "authentication.php";
+?>
+
+<!-- header -->
+<?php
+include "includes/header.php";
+?>
+
+<div class="container-fluid px-4">
+    <h4 class="mt-4">Groups</h4>
+
+    <div class="row mt-4">
+
+        <div class="col-md-12">
+            <?php include('message.php'); ?>
+            <div class="card">
+                <div class="card-header">
+                    <h4>View Groups
+                        <a href="group-add.php" class="btn btn-primary float-end">Add Group</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-stripe">
+                            <thead>
+                                <tr>
+                                    <th>Group Id</th>
+                                    <th>Assignment Title</th>
+                                    <th>Group Number</th>
+                                    <th>Status</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $groups = "SELECT * FROM groups";
+                                $groups_run = mysqli_query($connection, $groups);
+
+                                if (mysqli_num_rows($groups_run) > 0) {
+                                    foreach ($groups_run as $item) {
+                                ?>
+                                        <tr>
+                                            <td><?= $item['group_id'] ?></td>
+                                            <!-- <td><?= $item['assignment_id'] ?></td> -->
+                                            <td><?php 
+                                        $query = mysqli_query($connection, "SELECT assignment_title From assignments WHERE assignment_id='{$item['assignment_id']}' ");
+                                        while ($row = mysqli_fetch_array($query)){
+                                        $assignment_title = $row['assignment_title'] ;
+                                        echo $assignment_title;
+                                        }
+                                        ?></td> 
+                                            <td><?= $item['group_number'] ?></td>
+                                            <td>
+                                                <?= $item['status'] == '1' ? 'Hidden':'Visible' ?>
+                                            </td>
+                                            <td><a href="group-edit.php?group_id=<?= $item['group_id'] ?>" class="btn btn-info">Edit</td>
+                                            <td>
+                                                <form action="code.php" method="POST">
+                                                <button href="submit" name="group_delete" value="<?= $item['group_id'] ?>" class="btn btn-danger">Delete</button>
+                                            </form>
+                                                
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                } else {
+                                    ?>
+                                    <tr>
+                                        <td colspan="6">No Record Found</td>
+                                    </tr>
+                                <?php
+                                }
+
+
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- footer -->
+
+<?php
+include "includes/footer.php";
+?>
+
+<!-- scripts -->
+
+<?php
+include "includes/scripts.php";
+?>
