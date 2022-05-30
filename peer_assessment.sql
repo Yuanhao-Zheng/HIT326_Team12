e3a716b3457f1f6880622dc218f9cc385c5c5c6a
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-05-30 13:08:02 +0930

    updated sql

--- peer_assessment.sql
+++ peer_assessment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2022 at 02:58 PM
+-- Generation Time: May 30, 2022 at 05:37 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -41,14 +41,13 @@ CREATE TABLE `assignments` (
 --
 
 INSERT INTO `assignments` (`assignment_id`, `unit_id`, `assignment_title`, `navbar_status`, `status`, `created_at`) VALUES
-(6, 1, 'Assignment 2', 1, 1, '2022-05-18 13:56:15'),
-(7, 2, 'Assignment 1', 0, 0, '2022-05-18 13:56:15'),
-(12, 2, 'Assignment 1', 0, 0, '2022-05-18 14:32:06'),
-(13, 6, 'Assignment 3', 1, 1, '2022-05-18 14:33:35'),
-(16, 6, 'Assignment 123', 1, 1, '2022-05-18 14:35:41'),
-(17, 6, 'Assignment 374', 1, 1, '2022-05-18 14:37:07'),
-(30, 2, 'Assignment 12123', 1, 1, '2022-05-22 06:07:54'),
-(31, 8, 'Final Report', 1, 1, '2022-05-24 13:47:41');
+(32, 12, 'Data science group project 1', 0, 1, '2022-05-29 11:10:23'),
+(33, 12, 'Data science group project 2', 0, 1, '2022-05-29 11:10:34'),
+(34, 11, 'Simulation Project', 0, 1, '2022-05-29 11:11:05'),
+(35, 13, 'Written Assignment', 0, 1, '2022-05-29 11:11:32'),
+(36, 13, 'Software Project', 0, 1, '2022-05-29 11:11:40'),
+(37, 10, 'Group Project proposal', 0, 1, '2022-05-29 11:12:08'),
+(38, 10, 'Group Web application report', 0, 1, '2022-05-29 11:12:18');
 
 -- --------------------------------------------------------
 
@@ -70,17 +69,18 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`group_id`, `assignment_id`, `group_number`, `navbar_status`, `status`, `created_at`) VALUES
-(2, 17, 3, 1, 0, '2022-05-19 12:38:35'),
-(3, 12, 2, 0, 1, '2022-05-19 13:30:34'),
-(5, 16, 10, 1, 1, '2022-05-22 06:22:03'),
-(6, 16, 9, 1, 1, '2022-05-22 06:13:46'),
-(7, 17, 5, 1, 1, '2022-05-22 06:29:13'),
-(8, 13, 1, 1, 1, '2022-05-24 14:58:49'),
-(9, 6, 3, 1, 1, '2022-05-24 23:14:36'),
-(10, 6, 4, 1, 1, '2022-05-24 23:14:54'),
-(11, 17, 10, 1, 1, '2022-05-24 23:22:46'),
-(12, 17, 8, 1, 1, '2022-05-24 23:23:03'),
-(13, 16, 2, 1, 1, '2022-05-24 23:25:45');
+(15, 32, 1, 0, 1, '2022-05-29 11:13:03'),
+(16, 32, 2, 0, 1, '2022-05-29 11:13:07'),
+(17, 32, 3, 0, 1, '2022-05-29 11:13:11'),
+(18, 33, 1, 0, 1, '2022-05-29 11:13:23'),
+(19, 33, 2, 0, 1, '2022-05-29 11:13:30'),
+(20, 33, 3, 0, 1, '2022-05-29 11:13:35'),
+(21, 34, 4, 0, 1, '2022-05-29 11:13:52'),
+(22, 34, 5, 0, 1, '2022-05-29 11:14:01'),
+(23, 34, 6, 0, 1, '2022-05-29 11:14:06'),
+(24, 36, 10, 0, 1, '2022-05-29 11:14:20'),
+(25, 38, 8, 0, 0, '2022-05-29 11:14:31'),
+(26, 38, 9, 0, 1, '2022-05-29 11:14:37');
 
 -- --------------------------------------------------------
 
@@ -99,10 +99,13 @@ CREATE TABLE `joins` (
 --
 
 INSERT INTO `joins` (`join_id`, `student_id`, `group_id`) VALUES
-(1, 's123333', 3),
-(2, 's123456', 3),
-(3, 's312005', 3),
-(4, 's322698', 3);
+(8, 's312005', 15),
+(9, 's322698', 15),
+(10, 's319817', 15),
+(11, 's320346', 15),
+(12, 's322698', 21),
+(13, 's335834', 21),
+(14, 's343644', 21);
 
 -- --------------------------------------------------------
 
@@ -125,22 +128,31 @@ CREATE TABLE `reviews` (
 --
 
 INSERT INTO `reviews` (`review_id`, `join_id`, `criterion_1`, `criterion_2`, `criterion_3`, `criterion_4`, `submit_id`) VALUES
-(1, 1, 35, 35, 35, 35, 's123333'),
-(2, 1, 35, 35, 35, 35, 's123456'),
-(3, 1, 25, 25, 25, 25, 's312005'),
-(4, 1, 25, 25, 25, 25, 's322698'),
-(5, 2, 35, 35, 35, 35, 's123333'),
-(6, 2, 35, 35, 35, 35, 's123456'),
-(7, 2, 25, 25, 25, 25, 's312005'),
-(8, 2, 25, 25, 25, 25, 's322698'),
-(9, 3, 15, 15, 15, 15, 's123333'),
-(10, 3, 15, 15, 15, 15, 's123456'),
-(11, 3, 25, 25, 25, 25, 's312005'),
-(12, 3, 25, 25, 25, 25, 's322698'),
-(13, 4, 15, 15, 15, 15, 's123333'),
-(14, 4, 15, 15, 15, 15, 's123456'),
-(15, 4, 25, 25, 25, 25, 's312005'),
-(16, 4, 25, 25, 25, 25, 's322698');
+(35, 8, 35, 35, 35, 35, 's312005'),
+(36, 9, 35, 35, 35, 35, 's312005'),
+(37, 10, 15, 15, 15, 15, 's312005'),
+(38, 11, 15, 15, 15, 15, 's312005'),
+(39, 8, 35, 35, 35, 35, 's322698'),
+(40, 9, 35, 35, 35, 35, 's322698'),
+(41, 10, 15, 15, 15, 15, 's322698'),
+(42, 11, 15, 15, 15, 15, 's322698'),
+(43, 8, 25, 25, 25, 25, 's319817'),
+(44, 9, 25, 25, 25, 25, 's319817'),
+(45, 10, 25, 25, 25, 25, 's319817'),
+(46, 11, 25, 25, 25, 25, 's319817'),
+(47, 8, 25, 25, 25, 25, 's320346'),
+(48, 9, 25, 25, 25, 25, 's320346'),
+(49, 10, 25, 25, 25, 25, 's320346'),
+(50, 11, 25, 25, 25, 25, 's320346'),
+(51, 12, 34, 34, 34, 34, 's322698'),
+(52, 13, 33, 33, 33, 33, 's322698'),
+(53, 14, 33, 33, 33, 33, 's322698'),
+(54, 12, 34, 34, 34, 34, 's335834'),
+(55, 13, 33, 33, 33, 33, 's335834'),
+(56, 14, 33, 33, 33, 33, 's335834'),
+(57, 12, 34, 34, 34, 34, 's343644'),
+(58, 13, 33, 33, 33, 33, 's343644'),
+(59, 14, 33, 33, 33, 33, 's343644');
 
 -- --------------------------------------------------------
 
@@ -161,10 +173,12 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`student_id`, `student_firstname`, `student_lastname`, `status`, `created_at`) VALUES
-('s123333', 'Eric1234', '1234', 1, '2022-05-20 01:27:04'),
-('s123456', 'Eric ', 'Eric', 1, '2022-05-25 01:01:45'),
-('s312005', 'Michael', 'Abrahams', 1, '2022-05-26 02:21:48'),
-('s322698', 'Eric ', 'heng', 1, '2022-05-26 02:21:28');
+('s312005', 'Mike', 'Abrahams', 1, '2022-05-29 11:19:31'),
+('s319817', 'Kenzie', 'Chandra', 1, '2022-05-29 11:20:17'),
+('s320346', 'Gede Tomi Adi', 'Wirnaya', 1, '2022-05-29 11:21:02'),
+('s322698', 'Eric', 'Heng', 1, '2022-05-29 11:18:37'),
+('s335834', 'Rupesh', 'Shrestha', 1, '2022-05-29 11:18:22'),
+('s343644', 'Hoang Phong', 'Tran', 1, '2022-05-29 11:17:30');
 
 -- --------------------------------------------------------
 
@@ -188,11 +202,11 @@ CREATE TABLE `units` (
 --
 
 INSERT INTO `units` (`unit_id`, `unit_code`, `unit_name`, `unit_year`, `unit_semester`, `navbar_status`, `status`, `created_at`) VALUES
-(1, 'HIT226', 'Mobile Web Structure', 2012, 1, 1, 0, '2022-05-17 12:25:52'),
-(2, 'HIT237', 'Building Interactive Website', 2021, 0, 1, 0, '2022-05-17 12:26:37'),
-(3, 'HIT339', 'Distributed Development', 2012, 0, 1, 0, '2022-05-17 12:27:19'),
-(6, 'HIT374', 'Networking', 2013, 0, 1, 1, '2022-05-17 13:21:10'),
-(8, 'HIT353', 'Business Intelligence and Data Mining', 2020, 1, 1, 1, '2022-05-22 11:40:50');
+(9, 'HIT339', 'Distributed Development', 2022, 0, 1, 1, '2022-05-29 11:05:09'),
+(10, 'HIT326', 'Database-Driven Web Application', 2022, 1, 1, 1, '2022-05-29 11:06:29'),
+(11, 'HIT274', 'Network Engineering Applications', 2022, 0, 1, 1, '2022-05-29 11:07:20'),
+(12, 'HIT140', 'Foundations of Data Science', 2022, 0, 1, 1, '2022-05-29 11:07:47'),
+(13, 'PRT585', 'Software Engineering Practice', 2022, 0, 1, 1, '2022-05-29 11:08:31');
 
 -- --------------------------------------------------------
 
@@ -209,19 +223,16 @@ CREATE TABLE `users` (
   `user_email` varchar(255) NOT NULL,
   `user_role` tinyint(4) NOT NULL DEFAULT 0,
   `status` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `randSalt` varchar(255) NOT NULL
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_role`, `status`, `created_at`, `randSalt`) VALUES
-(1, '', '123', 'abc', 'abc', 'abc@abc.com', 1, 0, '2022-05-15 12:31:23', ''),
-(2, '', '123', 'abc1', 'abc1', 'abc1@bac1.com', 0, 0, '2022-05-15 12:31:58', ''),
-(3, '', 'abcd', 'abcd', 'abcde', 'abcd@abcd.com', 0, 0, '2022-05-15 12:40:44', ''),
-(4, '', '123', 'abc', 'abc', 'abcd@abcd.coma', 1, 0, '2022-05-15 12:43:13', '');
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_role`, `status`, `created_at`) VALUES
+(12, '', '$2y$12$rTwrCL/XwXLt/C9Tnm7xB.0JWEK7N2wDWvU7zyIJiCh3waUHaOFby', 'Stephen', 'Curry', 'admin@admin.com', 1, 0, '2022-05-29 05:56:23'),
+(13, '', '$2y$12$JmlFtAymm0Klk4nQDKFiuONMYfnQKi470LTwb7GkqRlfV8fbjZ/dG', 'Lebron', 'James', 'lecturer@lecturer.com', 0, 0, '2022-05-29 05:58:18');
 
 --
 -- Indexes for dumped tables
@@ -282,37 +293,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `joins`
 --
 ALTER TABLE `joins`
-  MODIFY `join_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `join_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `units`
 --
 ALTER TABLE `units`
-  MODIFY `unit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `unit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
